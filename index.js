55ef7586ad7ee927212f3281ecccb72ef668d98b
Surfsol <surfsol@protonamil.com>
2019-10-28 04:55:50 -0300

    initial commit

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // implement your API here
+const express = require('express')
+
+const server = express()
+
+const db = require('./data/db')
+
+//so express can read .json in request body
+server.use(express.json())
+
+server.get()
+
+
+const port = 7000
+server.listen(port, () => console.log(`\n** API on port ${port} **\n`))
